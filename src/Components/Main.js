90c96ccce1584f36aa6c64e079415b047e76caf6
Dheeraj Ahuja <dheeraj.ahuja@sap.com>
2022-07-05 23:15:55 +0530

    added css and final touches

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,21 +4,31 @@ import ReactDOM from "react-dom";
 export default function Main(props) {
   console.log(props);
   return (
-    <div className="card">
-      <img src={props.imageUrl} className="card-img"/>
-      <div>
-        <img src="../Images/pin.png" width="28px" height="30px" />
+    <div>
+      <div className="card">
+        <img src={props.imageUrl} className="card-img" />
+        <div className="card-details">
+          <div className="card-location">
+            <img
+              className="pin-location"
+              src="../Images/pin.png"
+              width="28px"
+              height="30px"
+            />
+            <h4>{props.location}</h4>
+            <p>
+              <a href={props.googleMapsUrl}>View on Google Maps</a>
+            </p>
+          </div>
 
-        <h4>{props.location}</h4>
-        <p>
-          <a href={props.googleMapsUrl}>View on Google Maps</a>
-        </p>
-        <h1>{props.title}</h1>
-        <h4>
-          {props.startDate} - {props.endDate}
-        </h4>
-        <p>{props.description}</p>
+          <h1>{props.title}</h1>
+          <h4>
+            {props.startDate} - {props.endDate}
+          </h4>
+          <p>{props.description}</p>
+        </div>
       </div>
+      <hr></hr>
     </div>
   );
 }
